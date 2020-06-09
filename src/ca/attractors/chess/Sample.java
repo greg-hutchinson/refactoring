b61ef09dcd0b88371a5914a95206423438e4d85f
Unknown <greg.hutchinson@fcc-fac.ca>
2020-06-09 13:20:59 -0600

    Refactored Attempt1

--- src/ca/attractors/chess/Sample.java
+++
@@ -1,9 +0,0 @@
-package ca.attractors.chess;
-
-public class Sample {
-    public static void main(String[] args) {
-        char x = 'h';
-        int off = x - 'a';
-        System.out.println("The value is " + off);
-    }
-}

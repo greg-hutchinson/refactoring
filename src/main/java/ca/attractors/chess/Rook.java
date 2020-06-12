970e8c4735317cb2875338ab38e9cdc202ddf458
Unknown <greg.hutchinson@fcc-fac.ca>
2020-06-12 16:44:34 -0600

    Refactor - Extract method on moveTo

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -9,36 +9,34 @@ public class Rook extends ChessPiece {
     }
 
     public boolean moveTo(Position targetPosition) {
-        Position currentPosition = getPosition();
-        //same row or same column?
-        if (targetPosition.x != currentPosition.x && targetPosition.y != currentPosition.y) {
+        if (isInvalidMoveTo(targetPosition))
             return false;
+        getChessboard().movePieceTo(this, targetPosition);
+        return true;
+    }
+
+    private boolean isInvalidMoveTo(Position targetPosition) {
+        if (isInvalidTarget(targetPosition)) return true;
+        if (isInvalidVerticalMove(targetPosition)) return true;
+        if (isInvalidHorizontalMove(targetPosition)) return true;
+        return false;
+    }
+
+    private boolean isInvalidTarget(Position targetPosition) {
+        if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
+            return true;
         }
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
         if (targetPiece != null) {
             if (targetPiece.getColor() == getColor())
-                return false;
+                return true;
         }
-        if (targetPosition.x == currentPosition.x) {
-            int start = currentPosition.getYOffset();
-            int end = targetPosition.getYOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int y = start+increment; y != end; y = y + increment) {
-                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
-            }
-        }
-        if (targetPosition.y == currentPosition.y) {
-            int start = currentPosition.getXOffset();
+        return false;
+    }
+
+    private boolean isInvalidHorizontalMove(Position targetPosition) {
+        if (targetPosition.y == getPosition().y) {
+            int start = getPosition().getXOffset();
             int end = targetPosition.getXOffset();
             int increment = 0;
             if (start > end)
@@ -51,12 +49,33 @@ public class Rook extends ChessPiece {
             }
             for (Position position: positions) {
                 if (getChessboard().getPieceAt(position) != null) {
-                    return false;
+                    return true;
                 }
             }
         }
-        getChessboard().movePieceTo(this, targetPosition);
-        return true;
+        return false;
+    }
+
+    private boolean isInvalidVerticalMove(Position targetPosition) {
+        if (targetPosition.x == getPosition().x) {
+            int start = getPosition().getYOffset();
+            int end = targetPosition.getYOffset();
+            int increment = 0;
+            if (start > end)
+                increment = -1;
+            else
+                increment = 1;
+            List<Position> positions = new ArrayList<>();
+            for (int y = start+increment; y != end; y = y + increment) {
+                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+            }
+            for (Position position: positions) {
+                if (getChessboard().getPieceAt(position) != null) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }

a70a8d241c64a3a05c10dae25f7b1b997dbe3732
Unknown <greg.hutchinson@fcc-fac.ca>
2020-06-12 15:06:19 -0600

    Rook horizontal test cases working

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -28,9 +28,26 @@ public class Rook extends ChessPiece {
             else
                 increment = 1;
             List<Position> positions = new ArrayList<>();
-            for (int y = start; y != end; y = y + increment) {
-                if (y != start && y != end)    //Code coverage should spot something here
-                    positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+            for (int y = start+increment; y != end; y = y + increment) {
+                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+            }
+            for (Position position: positions) {
+                if (getChessboard().getPieceAt(position) != null) {
+                    return false;
+                }
+            }
+        }
+        if (targetPosition.y == currentPosition.y) {
+            int start = currentPosition.getXOffset();
+            int end = targetPosition.getXOffset();
+            int increment = 0;
+            if (start > end)
+                increment = -1;
+            else
+                increment = 1;
+            List<Position> positions = new ArrayList<>();
+            for (int x = start+increment; x != end; x = x + increment) {
+                positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
             }
             for (Position position: positions) {
                 if (getChessboard().getPieceAt(position) != null) {

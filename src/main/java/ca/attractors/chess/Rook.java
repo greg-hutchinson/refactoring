e50a4aa6d1383bc7bf2d58efb2dfb6782eff4280
Unknown <greg.hutchinson@fcc-fac.ca>
2020-06-17 09:20:57 -0600

    Added some comments

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -8,17 +8,25 @@ public class Rook extends ChessPiece {
         super(chessboard, color);
     }
 
-
+    /**
+     *
+     * @param targetPosition - the position that we would like to move to
+     * @return true if we were able to complete the move. false otherwise
+     */
     public boolean moveTo(Position targetPosition) {
-        //same row or same column?
+        //if it is not the same x or y coordinate it is not a rooks valid move at all
         if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
             return false;
         }
+        //Next - Check to make sure that if the target square is occupied it is not the same color
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
         if (targetPiece != null) {
             if (targetPiece.getColor() == getColor())
                 return false;
         }
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
+        // the increment might be positive or negative.
         if (targetPosition.x == getPosition().x) {
             int start = getPosition().getYOffset();
             int end = targetPosition.getYOffset();
@@ -37,6 +45,9 @@ public class Rook extends ChessPiece {
                 }
             }
         }
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
+        // the increment might be positive or negative.
         if (targetPosition.y == getPosition().y) {
             int start = getPosition().getXOffset();
             int end = targetPosition.getXOffset();
@@ -55,6 +66,7 @@ public class Rook extends ChessPiece {
                 }
             }
         }
+        //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
     }

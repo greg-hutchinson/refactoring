a970b6bfe3b71bfbdfcc70b46c3f2f17e85e1c65
Hutch <greg.hutchinson@fcc-fac.ca>
2020-09-24 17:08:45 -0600

    Final

--- src/main/java/ca/attractors/accounting/Customer.java
+++ src/main/java/ca/attractors/accounting/Customer.java
@@ -22,51 +22,57 @@ public class Customer {
     public void addAccount(Account account) {
         accounts.add(account);
     }
+    
     @Override
     public String toString() {
+        return new CustomerString().toString();
+    }
+
+    class CustomerString {
         StringBuffer buffer = new StringBuffer();
-        buffer.append("Summary for customer: " + name + "\n");
         String currentAccountType = null;
-        for (Account account: getSortedAccounts()) {
-            currentAccountType = possibleHeadingBreak(buffer, currentAccountType, account);
-            appendAccountTo(account, buffer);
-        }
-        return buffer.toString();
-    }
 
-    private String possibleHeadingBreak(StringBuffer buffer, String currentAccountType, Account account) {
-        if (!account.getType().equals(currentAccountType)) {
-            currentAccountType = account.getType();
-            buffer.append("\n\n******************  " + currentAccountType + "  *******\n");
-        }
-        return currentAccountType;
-    }
-
-    private void appendAccountTo(Account account, StringBuffer buffer) {
-        buffer.append("\tAccount:" + account.getNumber() + "\n");
-        for (Transaction transaction: account.getTransactions()) {
-            appendTransactionTo(transaction, buffer);
-        }
-    }
-
-    private void appendTransactionTo(Transaction transaction, StringBuffer buffer) {
-        if (transaction.getType().equals("CREDIT")) {
-            buffer.append("\t\tTransaction:\t\t-" + transaction.getAmount() + "\n");
-        }
-        else {
-            buffer.append("\t\tTransaction:\t\t\t\t" + transaction.getAmount() + "\n");
-        }
-    }
-
-    private List<Account> getSortedAccounts() {
-        List <Account> sortedAccounts = new ArrayList<>();
-        sortedAccounts.addAll(accounts);
-        sortedAccounts.sort(new Comparator<Account>() {
-            @Override
-            public int compare(Account o1, Account o2) {
-                return o1.getType().compareTo(o2.getType());
+        public String toString() {
+            buffer.append("Summary for customer: " + name + "\n");
+            for (Account account : getSortedAccounts()) {
+                headingBreakIfTypeChanged(account);
+                appendAccount(account);
+                currentAccountType = account.getType();
             }
-        });
-        return sortedAccounts;
+            return buffer.toString();
+        }
+
+        private void headingBreakIfTypeChanged(Account account) {
+            if (account.getType().equals(currentAccountType))
+                return;
+            buffer.append("\n\n******************  " + account.getType() + "  *******\n");
+        }
+
+        private void appendAccount(Account account) {
+            buffer.append("\tAccount:" + account.getNumber() + "\n");
+            for (Transaction transaction : account.getTransactions()) {
+                appendTransaction(transaction);
+            }
+        }
+
+        private void appendTransaction(Transaction transaction) {
+            String suffix = "\t\t";
+            if (transaction.getType().equals("CREDIT"))
+                suffix = "-";
+            //reduced duplication even within the one method.
+            buffer.append ("\t\tTransaction:\t\t" + suffix + transaction.getAmount() + "\n");
+        }
+
+        private List<Account> getSortedAccounts() {
+            List<Account> sortedAccounts = new ArrayList<>();
+            sortedAccounts.addAll(accounts);
+            sortedAccounts.sort(new Comparator<Account>() {
+                @Override
+                public int compare(Account o1, Account o2) {
+                    return o1.getType().compareTo(o2.getType());
+                }
+            });
+            return sortedAccounts;
+        }
     }
 }

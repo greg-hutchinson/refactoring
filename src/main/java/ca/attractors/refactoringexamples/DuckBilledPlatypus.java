7589c284631ad2cc9cbc527c94a2fa3e0c00efe5
Greg Hutchinson <the.greg.hutchinson@gmail.com>
2023-02-13 08:11:05 -0600

    Too complicated

--- src/main/java/ca/attractors/refactoringexamples/DuckBilledPlatypus.java
+++ src/main/java/ca/attractors/refactoringexamples/DuckBilledPlatypus.java
@@ -2,15 +2,5 @@ package ca.attractors.refactoringexamples;
 
 public class DuckBilledPlatypus extends Mammal {
 
-    public MyPlatypus getMyPlatypus() {
-        return new MyPlatypus();
-    }
-
-    // Poor Example - needs work.
-    class MyPlatypus extends Reptile {
-        public boolean isSomething() {
-            return isBornLive() && isUgly();
-        }
-
-    }
+    
 }
